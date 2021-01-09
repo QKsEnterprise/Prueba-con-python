2048f7ee9726756cc36ce4267d6fd07902dba9ed
Kuka <martinelliluca@gmail.com>
2021-01-09 16:37:01 -0300

    Agregue un programa para pagar con tarjeta de credito

---
+++ CreditCard.py
@@ -0,0 +1,19 @@
+NumberOne = 300 * 12
+Numbertwo = 100 * 10
+NumberTree = 500 * 5
+NumberFour = 200 * 10
+NumberFive = 50 * 58
+
+Addition = NumberOne + Numbertwo + NumberTree + NumberFour + NumberFive
+
+print(" Total: $", Addition)
+
+
+CreditCard = 1000000
+
+restar = CreditCard - Addition
+
+if CreditCard >= Addition:
+    print("Trancicion ¡Ok! $", restar)
+else:
+    print("You can't pay this pruduct/s")
