cdf69d124b368a255d0d724c876dadb96f56aa89
Kuka <martinelliluca@gmail.com>
2021-01-09 13:15:50 -0300

    lista de datos del cliente

---
+++ cart.py
@@ -0,0 +1,17 @@
+# lista de compras
+
+person = [
+    "Name: Jonh",
+    "LastName: Parkker",
+    "Number: 11-4758-8092",
+    "DNI: 31.583455",
+]
+
+cart = [
+    "bananas",
+    "apple",
+    "kiwi",
+    "oranjes"
+]
+
+print(person)
